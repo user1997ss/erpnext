36b2a095ada48e434e54a856d91c4af95b436663
Anas-bg <anasb@ns-apps.com>
2020-02-06 11:45:40 +0200

    second commit

--- erpnext/crm/doctype/sqade/sqade.js
+++ erpnext/crm/doctype/sqade/sqade.js
@@ -3,12 +3,7 @@
 
 frappe.ui.form.on('sqade', {
 	 refresh: function(frm) {
-<<<<<<< HEAD
-		console.log("gitlab");
-||||||| merged common ancestors
-		console.log("dsfss");
-=======
+		console.log("first")
 		console.log("github");
->>>>>>> ece6f89bf26940e5b6c0e46c4454b50d98b1406e
 	}
 });
